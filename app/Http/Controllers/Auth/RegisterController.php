3e64e86aa6132be18e7ecf28d77b47da15d77a34
Elie Andraos <elie@andraos.org>
2018-02-23 18:43:20 +0200

    Populate predefined categories upon user registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -62,10 +62,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+
+        $user->assignDefaultCategories();
+
+        return $user;
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Category;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -35,5 +36,36 @@ class User extends Authenticatable
     public function categories()
     {
         return $this->hasMany(\App\Models\Category::class);
+    }
+
+    /**
+     * Assigns predefined categories for the user.
+     * 
+     * @return type
+     */
+    public function assignDefaultCategories()
+    {
+        $categories = [];
+
+        $predefined = [
+            ['name' => 'Hair & Beauty', 'icon' => 'fas fa-venus-mars'],
+            ['name' => 'Bills', 'icon' => 'fas fa-dollar-sign'],
+            ['name' => 'Car', 'icon' => 'fas fa-car'],
+            ['name' => 'Clothes & Shoes', 'icon' => 'fas fa-shopping-bag'],
+            ['name' => 'Eating Out', 'icon' => 'fas fa-utensils'],
+            ['name' => 'Entertainment', 'icon' => 'fas fa-beer'],
+            ['name' => 'Fitness & Sports', 'icon' => 'fas fa-shopping-bag'],
+            ['name' => 'Gifts', 'icon' => 'fas fa-gift'],
+            ['name' => 'Health Care', 'icon' => 'fas fa-heartbeat'],
+            ['name' => 'Traveling', 'icon' => 'fas fa-plane'],
+            ['name' => 'House', 'icon' => 'fas fa-home'],
+            ['name' => 'Online subscriptions', 'icon' => 'fas fa-mouse-pointer'],
+        ];
+
+        foreach($predefined as $category){
+            array_push($categories, new Category($category));
+        }
+
+        $this->categories()->saveMany($categories);
     }
 }
